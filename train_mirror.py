9122195fe62b5e86177deb99d37e9006ff294657
taylor <mhy845879017@gmail.com>
2018-03-29 18:42:12 +0800

    mirror

--- train_mirror.py
+++ train_mirror.py
@@ -1,5 +1,4 @@
 # # Mask R-CNN - Train on Mirror Dataset
-
 import os
 import sys
 import random
@@ -7,17 +6,16 @@ import math
 import re
 import time
 import numpy as np
-import cv2
 import matplotlib
 import matplotlib.pyplot as plt
-import yaml
 
-from PIL import Image
 from config import Config
 import utils
 import model as modellib
 import visualize
 from model import log
+
+import mirror
 
 # Root directory of the project
 ROOT_DIR = os.getcwd()
@@ -30,121 +28,11 @@ COCO_MODEL_PATH = os.path.join(ROOT_DIR, "mask_rcnn_coco.h5")
 # Download COCO trained weights from Releases if needed
 if not os.path.exists(COCO_MODEL_PATH):
     utils.download_trained_weights(COCO_MODEL_PATH)
-
-
-# ## Configurations
-class MirrorConfig(Config):
-    """Configuration for training on the mirror dataset.
-    Derives from the base Config class and overrides values specific
-    to the mirror dataset.
-    """
-    # Give the configuration a recognizable name
-    NAME = "mirror"
-
-    # Train on 1 GPU and 8 images per GPU. We can put multiple images on each
-    # GPU because the images are small. Batch size is 8 (GPUs * images/GPU).
-    GPU_COUNT = 1
-    IMAGES_PER_GPU = 1
-
-    # Number of classes (including background)
-    NUM_CLASSES = 1 + 1  # background + 1 mirror
-
-    # Use small images for faster training. Set the limits of the small side
-    # the large side, and that determines the image shape.
-    IMAGE_MIN_DIM = 256
-    IMAGE_MAX_DIM = 256
-
-    # Use smaller anchors because our image and objects are small
-    RPN_ANCHOR_SCALES = (8, 16, 32, 64, 128)  # anchor side in pixels
-
-    # Reduce training ROIs per image because the images are small and have
-    # few objects. Aim to allow ROI sampling to pick 33% positive ROIs.
-    TRAIN_ROIS_PER_IMAGE = 10
-
-    # Use a small epoch since the data is simple
-    STEPS_PER_EPOCH = 100
-
-    # use small validation steps since the epoch is small
-    VALIDATION_STEPS = 5
     
-config = MirrorConfig()
+config = mirror.MirrorConfig()
 config.display()
 
 iter_num = 0
-
-# ## Dataset
-class MirrorDataset(utils.Dataset):
-
-    def get_obj_index(self, image):
-        """Get the number of instance in the image
-        """
-        n = np.max(image)
-        return n
-
-    def from_yaml_get_class(self,image_id):
-        """Translate the yaml file to get label """
-        info = self.image_info[image_id]
-        with open(info['yaml_path']) as f:
-            temp = yaml.load(f.read())
-            labels = temp['label_names']
-            del labels[0]
-        return labels
-
-    def draw_mask(self, num_obj, mask, image, image_id):
-        info = self.image_info[image_id]
-        for index in range(num_obj):
-            """j is row and i is column"""
-            for i in range(info['width']):
-                for j in range(info['height']):
-                    at_pixel = image.getpixel((i, j))
-                    if at_pixel == index +1:
-                        mask[j, i, index] = 1
-        return mask
-
-    def load_shapes(self, count, height, width, img_folder, mask_folder,
-                    imglist, dataset_root_path):
-        self.add_class("shapes", 1, "mirror")
-        # self.add_class("shapes", 2, "reflection")
-        for i in range(count):
-            filestr = imglist[i].split(".")[0]
-            # filestr = filestr.split("_")[1]
-            mask_path = mask_folder + "/" + filestr + "_json/label8.png"
-            yaml_path = mask_folder + "/" + filestr + "_json/info.yaml"
-            self.add_image("shapes", image_id=i, path=img_folder + "/" + imglist[i],
-                           width=width, height=height, mask_path=mask_path, yaml_path=yaml_path)
-
-
-    def load_mask(self, image_id):
-        global iter_num
-        info = self.image_info[image_id]
-        count = 1
-        img = Image.open(info['mask_path'])
-        width, height = img.size
-        num_obj = self.get_obj_index(img)
-        mask = np.zeros([info['height'], info['width'], num_obj], dtype=np.uint8)
-        mask = self.draw_mask(num_obj, mask, img, image_id)
-        occlusion = np.logical_not(mask[:, :, -1]).astype(np.uint8)
-        for i in range(count - 2, -1, -1):
-            mask[:, :, i] = mask[:, :, i] * occlusion
-            occlusion = np.logical_and(occlusion, np.logical_not(mask[:, :, i]))
-        labels=[]
-        labels=self.from_yaml_get_class(image_id)
-        labels_form=[]
-        for i in range(len(labels)):
-            if labels[i].find("mirror")!=-1:
-                #print "box"
-                labels_form.append("mirror")
-            # elif labels[i].find("column")!=-1:
-            #     #print "column"
-            #     labels_form.append("column")
-            # elif labels[i].find("package")!=-1:
-            #     #print "package"
-            #     labels_form.append("package")
-            # elif labels[i].find("fruit")!=-1:
-            #     #print "fruit"
-            #     labels_form.append("fruit")
-        class_ids = np.array([self.class_names.index(s) for s in labels_form])
-        return mask, class_ids.astype(np.int32)
 
 # Configuration
 dataset_root_path = "/home/taylor/Mask_RCNN/dataset/"
@@ -155,12 +43,12 @@ count = len(imglist)
 print(count)
 
 # Training dataset
-dataset_train = MirrorDataset()
+dataset_train = mirror.MirrorDataset()
 dataset_train.load_shapes(count, 200, 300, img_folder, mask_folder, imglist, dataset_root_path)
 dataset_train.prepare()
 
 # Validation dataset
-dataset_val = MirrorDataset()
+dataset_val = mirror.MirrorDataset()
 dataset_val.load_shapes(count, 200, 300, img_folder, mask_folder, imglist, dataset_root_path)
 dataset_val.prepare()
 
@@ -172,7 +60,6 @@ for image_id in image_ids:
     visualize.display_top_masks(image, mask, class_ids, dataset_train.class_names)
 
 ### Create Model  ###
-# Create model in training mode
 model = modellib.MaskRCNN(mode="training", config=config,
                           model_dir=MODEL_DIR)
 
@@ -194,31 +81,18 @@ elif init_with == "last":
 
 
 # ## Training
-#
-# Train in two stages:
-# 1. Only the heads. Here we're freezing all the backbone layers and training only the randomly initialized layers (i.e. the ones that we didn't use pre-trained weights from MS COCO). To train only the head layers, pass `layers='heads'` to the `train()` function.
-#
-# 2. Fine-tune all layers. For this simple example it's not necessary, but we're including it to show the process. Simply pass `layers="all` to train all layers.
-
 
 # 1. Train the head branches
-# Passing layers="heads" freezes all layers except the head
-# layers. You can also pass a regular expression to select
-# which layers to train by name pattern.
 model.train(dataset_train, dataset_val,
             learning_rate=config.LEARNING_RATE,
             epochs=20,
             layers='heads')
 
 # 2. Fine tune all layers
-# Passing layers="all" trains all layers. You can also
-# pass a regular expression to select which layers to
-# train by name pattern.
 # model.train(dataset_train, dataset_val,
 #             learning_rate=config.LEARNING_RATE / 10,
 #             epochs=2,
 #             layers="all")
-
 
 
 # Save weights
